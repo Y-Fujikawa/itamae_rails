bb51cafd7883bda5ca5e933494d5bb6c78047749
Y-Fujikawa <fujikawa0@gmail.com>
2015-09-08 00:20:28 +0900

    Add serverspec monit

---
+++ spec/vagrant.local/monit_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe package('monit') do
+  it { should be_installed }
+end
+
+describe service('monit') do
+  it { should be_enabled }
+  it { should be_running }
+end
